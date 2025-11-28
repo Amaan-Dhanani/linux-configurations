09d0cddbf8d09cf171068d31ccddd83d0ea36d35
Amaan-Dhanani <120526048+Amaan-Dhanani@users.noreply.github.com>
2025-11-28 15:34:40 -0500

    Update tile-left.sh

--- files/tile-left.sh
+++ files/tile-left.sh
@@ -2,9 +2,8 @@
 # Remove maximized/fullscreen state
 wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
 
-# Get screen dimensions
-SCREEN_W=$(xdpyinfo | awk '/dimensions:/ {print $2}' | cut -d'x' -f1)
-SCREEN_H=$(xdpyinfo | awk '/dimensions:/ {print $2}' | cut -d'x' -f2)
+# Get usable screen area (excluding panels)
+read X Y W H <<< $(xprop -root _NET_WORKAREA | awk '{print $3, $4, $5, $6}')
 
-# Tile active window to the left half, subtracting 38px from bottom
-wmctrl -r :ACTIVE: -e 0,0,0,$((SCREEN_W/2)),$((SCREEN_H-38))
+# Tile active window to the left half
+wmctrl -r :ACTIVE: -e 0,$X,$Y,$((W/2)),$H
