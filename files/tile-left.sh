57aeab9f198042eca22d73ecf6e928c2698b1acf
Amaan-Dhanani <120526048+Amaan-Dhanani@users.noreply.github.com>
2025-11-28 14:59:34 -0500

    Add tile-left.sh to resize active window to left half

---
+++ files/tile-left.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+SCREEN_W=$(xdpyinfo | awk '/dimensions:/ {print $2}' | cut -d'x' -f1)
+SCREEN_H=$(xdpyinfo | awk '/dimensions:/ {print $2}' | cut -d'x' -f2)
+wmctrl -r :ACTIVE: -e 0,0,0,$((SCREEN_W/2)),$SCREEN_H
