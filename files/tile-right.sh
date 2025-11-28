0039d3f9f8bd975fce9972c374e334b4d9543e7e
Amaan-Dhanani <120526048+Amaan-Dhanani@users.noreply.github.com>
2025-11-28 15:26:49 -0500

    Modify tiling to subtract 38px from window height
    Adjust active window tiling to account for bottom space.

--- files/tile-right.sh
+++ files/tile-right.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# Remove maximized/fullscreen state
 wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
+
+# Get screen dimensions
 SCREEN_W=$(xdpyinfo | awk '/dimensions:/ {print $2}' | cut -d'x' -f1)
 SCREEN_H=$(xdpyinfo | awk '/dimensions:/ {print $2}' | cut -d'x' -f2)
-wmctrl -r :ACTIVE: -e 0,$((SCREEN_W/2)),0,$((SCREEN_W/2)),$SCREEN_H
+
+# Tile active window to the right half, subtracting 38px from bottom
+wmctrl -r :ACTIVE: -e 0,$((SCREEN_W/2)),0,$((SCREEN_W/2)),$((SCREEN_H-38))
