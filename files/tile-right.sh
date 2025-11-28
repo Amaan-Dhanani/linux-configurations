128f6acbb0f8adfba6eb06a320de16d91ff419b9
Amaan-Dhanani <120526048+Amaan-Dhanani@users.noreply.github.com>
2025-11-28 15:06:14 -0500

    Update tile-right.sh

--- files/tile-right.sh
+++ files/tile-right.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
 SCREEN_W=$(xdpyinfo | awk '/dimensions:/ {print $2}' | cut -d'x' -f1)
 SCREEN_H=$(xdpyinfo | awk '/dimensions:/ {print $2}' | cut -d'x' -f2)
 wmctrl -r :ACTIVE: -e 0,$((SCREEN_W/2)),0,$((SCREEN_W/2)),$SCREEN_H
